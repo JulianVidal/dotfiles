e88dffd4b89c381b1d0375f338b84ede66abd9cf
Vidal Polania <julian.c.vidalpolania@bt.com>
2024-07-08 09:11:40 +0100

    To change windwos terminal settings it prompts user

--- windows_terminal/pull_settings.sh
+++ windows_terminal/pull_settings.sh
@@ -2,8 +2,13 @@
 
 set -eu -o pipefail # fail on error and report it, debug all lines
 
+if [ -z "$1" ]; then
+	echo "Please supply a username"
+	exit 1
+fi
+
 # Directory paths
-WINTERM=/mnt/c/Users/JuliV/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+WINTERM=/mnt/c/Users/$1/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
 
 WINTERMDOT=~/dotfiles/windows_terminal/settings.json
 
